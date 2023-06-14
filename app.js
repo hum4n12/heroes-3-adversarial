fb73f0e892b95dbd858c72488c1b099708a48520
hum4n12 <hum4n12@protonmail.com>
2023-06-14 18:29:03 +0200

    added logger

--- app.js
+++ app.js
@@ -2,9 +2,23 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const fs = require('fs');
 const path = require('path');
+const winston = require('winston');
 const modelManager = require('./modelManager');
 const app = express();
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 3000;
+
+const logger = winston.createLogger({
+  level: 'info',
+  format: winston.format.json(),
+  defaultMeta: { service: 'user-service' },
+  transports: [
+    new winston.transports.File(
+      { filename: 'log.log', format: winston.format.combine(
+      winston.format.timestamp(),
+      winston.format.json()
+    )})
+  ],
+});
 
 app.use(express.static(__dirname + '/public'));
 
@@ -13,7 +27,7 @@ app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
   res.send('index.html');
-})
+});
 
 app.post('/generate/aboveground', async (req, res) => {
   return await prepareRequest(req, res, modelManager.generateAboveground);
@@ -27,12 +41,17 @@ const prepareRequest = async (req, res, generateFunction) => {
   if (!req.body || !req.body.file) {
     return res.status(400).send();
   }
-  const image = req.body.file.replace(/^data:image\/jpeg;base64,/, "");
-  const buffer = Buffer.from(image, 'base64');
-  const generatedImage = await generateFunction(buffer);
-  const generatedImagePath = path.resolve('./public/img/generated_maps/generated.jpeg');
-  fs.writeFileSync(generatedImagePath, generatedImage);
-  return res.sendFile(generatedImagePath);
+  try {
+    const image = req.body.file.replace(/^data:image\/jpeg;base64,/, "");
+    const buffer = Buffer.from(image, 'base64');
+    const generatedImage = await generateFunction(buffer);
+    const generatedImagePath = path.resolve('./public/img/generated_maps/generated.jpeg');
+    fs.writeFileSync(generatedImagePath, generatedImage);
+    return res.sendFile(generatedImagePath);
+  } catch (error) {
+    logger.log('error', error.message);
+    return res.send('error');
+  }
 }
 
 app.listen(port, () => {
